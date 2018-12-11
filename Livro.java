a71e07ce8332f13474f00126278d1316c52ac92a
Gregory Santos <gregory.sgsantos@gmail.com>
2018-12-10 23:11:16 -0300

    Adicionada classe Exemplar Classe Livro modificada para ter um vetor de Exemplar

--- Livro.java
+++ Livro.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Livro{
 
     private String titulo;
@@ -9,6 +11,22 @@ public class Livro{
 	private Usuario dono;
 	private int devolucao;
 	private int reserva;
+	private ArrayList<Exemplar> exemplares;
+	
+	public Livro(String titulo, int codigo, String editora, String autores, int edicao, int anoPubli) {
+		this.titulo = titulo;
+		this.codigo = codigo;
+		this.editora = editora;
+		this.autores = autores;
+		this.edicao = edicao;
+		this.anoDaPublicacao = anoPubli;
+		exemplares = new ArrayList<Exemplar>();
+	}
+	
+	public void addExemplar(int codigoExemplar, String status) {
+		Exemplar exemplar = new Exemplar(this, codigoExemplar, status);
+		exemplares.add(exemplar);
+	}
 
 	public Usuario getDono() {
 		return this.dono;
